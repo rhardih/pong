bd3d8be04573148c5406de3fea268d5100fc5f89
René Hansen <renehh@gmail.com>
2019-02-05 15:55:42 +0100

    Sets root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "checks#index"
+
   resources :checks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
