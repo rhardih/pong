1a28d4b331e06a77ccee9261c9f9d9a4cb08cbc8
René Hansen <renehh@gmail.com>
2019-02-05 18:11:55 +0100

    Updates notice and error message boxes
    Adds proper semantic-ui classes.

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,5 +1,10 @@
 class Check < ApplicationRecord
   def self.protocols
-    [:https, :http]
+    ["https", "http"]
   end
+
+  validates :name, presence: true
+  validates :interval, numericality: { only_integer: true }
+  validates :protocol, inclusion: { in: protocols }
+  validates :url, presence: true
 end
