bdbd110738472adc41f447480cc81cc518227d7b
René Hansen <renehh@gmail.com>
2019-02-11 13:51:43 +0100

    Adds active_link_to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def active_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options[:class] << " active" if current_page?(options)
+    link_to(name, options, html_options, &block)
+  end
 end
