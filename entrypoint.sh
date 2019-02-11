35669ae7f606c84d73a1b58a8f450ed7e8263648
René Hansen <renehh@gmail.com>
2019-02-11 12:30:47 +0100

    Updates path name of project for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /pong/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
