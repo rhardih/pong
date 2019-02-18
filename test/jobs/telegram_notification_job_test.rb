15a556884d02c19b179c934129614ffc82a0a504
René Hansen <renehh@gmail.com>
2019-02-18 15:22:16 +0100

    Adds TelegramNotificationJob

---
+++ test/jobs/telegram_notification_job_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class TelegramNotificationJobTest < ActiveJob::TestCase
+  fixtures :checks
+
+  def setup
+    ENV['TELEGRAM_API_KEY'] = 'foo'
+    ENV['TELEGRAM_CHAT_ID'] = 'bar'
+  end
+
+  test "perform up notification" do
+    check = checks(:up)
+    ts = check.pings.first.created_at
+
+    stub_request(:post, "https://api.telegram.org/botfoo/sendMessage").with(
+      body: {
+        chat_id: "bar",
+        text: "Pong Alert\n\nUp is up again at #{ts}, after 5 minutes of downtime.\n"
+      }).to_return(status: 200, body: "{}")
+
+    TelegramNotificationJob.perform_now(check, available: true)
+
+    assert_requested :post, "https://api.telegram.org/botfoo/sendMessage"
+  end
+
+  test "perform down notification" do
+    check = checks(:up)
+    ts = check.pings.first.created_at
+
+    stub_request(:post, "https://api.telegram.org/botfoo/sendMessage").with(
+      body: {
+        chat_id: "bar",
+        text: "Pong Alert\n\nUp is down.\n"
+      }).to_return(status: 200, body: "{}")
+
+    TelegramNotificationJob.perform_now(check, available: false)
+
+    assert_requested :post, "https://api.telegram.org/botfoo/sendMessage"
+  end
+end
