48ba20db85ceab96002e54b6224e34e40d783143
René Hansen <renehh@gmail.com>
2019-03-06 11:36:14 +0100

    Adds example check/pings to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+check = Check.create(name: 'Example', interval: 1, protocol: 'https', url: 'example.com')
+
+# Create pings for the last seven days
+date = 7.days.ago.beginning_of_day
+# response time
+#rt = 250
+now = Time.now
+
+spike = lambda do |rt, &block|
+  # 10% chance of a spike
+  rt += rand(150..250) if rand(0..9) < 1
+
+  block.call(rt)
+end
+
+while date < now
+  spike.call(250 + rand(-25..25)) do |rt|
+    Ping.create(check: check, response_time: rt, created_at: date)
+  end
+
+  date = date.advance(minutes: 1)
+end
