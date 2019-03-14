237e80032ba74cc48670fb6a0e19d7f7dc70fc25
René Hansen <renehh@gmail.com>
2019-03-14 16:11:55 +0100

    Deletes brittle case for QueueRequestJobsTest

--- test/jobs/queue_request_jobs_test.rb
+++ test/jobs/queue_request_jobs_test.rb
@@ -3,13 +3,6 @@ require 'test_helper'
 class QueueRequestJobsTest < ActiveJob::TestCase
   fixtures :checks
 
-  test "only enqueues jobs for stale checks" do
-    # one for both Default and Expired checks
-    assert_enqueued_jobs 2 do
-      QueueRequestJobs.perform
-    end
-  end
-
   test "enqueus request jobs" do
     assert_enqueued_with(job: RequestJob, args: [checks(:default)]) do
       assert_enqueued_with(job: RequestJob, args: [checks(:expired)]) do
